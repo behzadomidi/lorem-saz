b368a233e3309af6e93ff7885fd7cee52d5b97ae
behzad <121344556+behzadomidi@users.noreply.github.com>
2023-04-24 09:19:06 +0330

    final

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import "./App.css";
+import data from "./data";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 1) {
+      amount = 1;
+    }
+    if (count > 4) {
+      amount = 4;
+    }
+    setText(data.slice(0, amount));
+  };
+
+  return (
+    <div className="section-center">
+      <h4>لورم ایپسوم ساز </h4>
+      <form className="lorem" onSubmit={handleSubmit}>
+        <label htmlFor="total"> تعداد پاراگراف : </label>
+        <input
+          type="number"
+          name="total"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit">بساز</button>
+      </form>
+      <article className="lorem-text">
+        {text && text.map((item, index) => <p key={index}>{item}</p>)}
+      </article>
+    </div>
+  );
+}
+
+export default App;
